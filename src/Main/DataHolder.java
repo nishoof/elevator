051789aadc72da3ba0f4eb76ab4e246c0ceaaf14
nishoof <88177986+nishoof@users.noreply.github.com>
2025-03-29 18:10:07 -0700

    Refactor DataHolder (remove unused setters and add private constructor

--- src/Main/DataHolder.java
+++ src/Main/DataHolder.java
@@ -5,44 +5,39 @@ import processing.core.PConstants;
 import processing.core.PFont;
 import processing.core.PImage;
 
-public class DataHolder {
+/**
+ * Utility class for managing fonts and images in the data folder.
+ */
+public final class DataHolder {
 
     private static PFont regularFont;
     private static PFont mediumFont;
 
     private static PImage heartImg;
 
-    public static void init(PApplet d) {
-        heartImg = d.loadImage("images/heart.png");
-        heartImg.resize(50, 50);
+    // Private constructor to prevent instantiation
+    private DataHolder() {
+    }
 
+    public static void init(PApplet d) {
         regularFont = d.createFont("fonts/GeistMono-Regular.otf", 128);
         mediumFont = d.createFont("fonts/GeistMono-Medium.otf", 128);
         d.textMode(PConstants.MODEL);
+
+        heartImg = d.loadImage("images/heart.png");
+        heartImg.resize(50, 50);
     }
 
     public static PFont getRegularFont() {
         return regularFont;
     }
 
-    public static void setRegularFont(PFont regular) {
-        DataHolder.regularFont = regular;
-    }
-
     public static PFont getMediumFont() {
         return mediumFont;
     }
 
-    public static void setMediumFont(PFont medium) {
-        DataHolder.mediumFont = medium;
-    }
-
     public static PImage getHeartImg() {
         return heartImg;
-    }
-
-    public static void setHeartImg(PImage heartImg) {
-        DataHolder.heartImg = heartImg;
     }
 
 }
